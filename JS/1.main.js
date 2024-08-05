e93c262266029e54b440d874bf16a546487f22fc
Baibhab Shree <2004bshree@gmail.com>
2024-08-05 17:36:33 +0530

    JS

---
+++ JS/1.main.js
@@ -0,0 +1,7 @@
+// console.log("Hello World");
+
+// window.alert(`This is an alert!`)
+
+document.getElementById("myh1").textContent = "Hello!";
+
+document.getElementById("myp").textContent = "I like Pizza!";
