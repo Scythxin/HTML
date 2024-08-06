b4770e12f5fa6cb0902eab0f827dc12ff4a4b885
Baibhab Shree <2004bshree@gmail.com>
2024-08-06 19:03:02 +0530

    Arith

---
+++ JS/3/main.js
@@ -0,0 +1,28 @@
+let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// students = students % 2;
+
+//Augmented assigned operators
+// students += 1; 
+// students -= 1; 
+// students *= 2; 
+// students /= 2; 
+// students **= 2; 
+// students %= 2;
+
+// students++;
+// students--;
+
+// Operator Precedence
+// 1. parenthesis()
+// 2. exponents
+// 3. multiplication, division and modulo
+// 4. addition and substraction
+
+
+console.log(students);
