2c08a6f3da4cbe8618c3c02a83e18b7315c2b336
Baibhab Shree <2004bshree@gmail.com>
2024-08-15 12:34:18 +0530

    Js

--- JS/4.User_inp/main.js
+++ JS/4.User_inp/main.js
@@ -13,3 +13,6 @@ document.getElementById("mySubmit").onclick = function(){
     username = document.getElementById("myText").value;
     document.getElementById("myh1").textContent = `Hello ${username}`;
 }
+
+//const : Used as a constant, its value never changes
+//Ex- const PI = 3.14
