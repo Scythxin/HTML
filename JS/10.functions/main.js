159e7b814a2f472b28d827aa492c5d70824258da
Baibhab Shree <2004bshree@gmail.com>
2024-08-16 21:01:06 +0530

    function

---
+++ JS/10.functions/main.js
@@ -0,0 +1,9 @@
+function happyBirthday(username,age){
+    console.log("Happy Birthday to you!");
+    console.log("Happy Birthday to you!");
+    console.log(`Happy Birthday dear ${username}!`);
+    console.log("Happy Birthday to you!");
+    console.log(`Happy Birthday, you are ${age} years old.`);
+}
+
+happyBirthday("Spongebob", 30);
