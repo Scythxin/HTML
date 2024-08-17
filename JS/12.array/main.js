c7e92a68133d62f976ab307625a79ebcb536cd0a
Baibhab Shree <2004bshree@gmail.com>
2024-08-17 16:51:28 +0530

    Array

---
+++ JS/12.array/main.js
@@ -0,0 +1,29 @@
+let fruits = ["apple", "orange", "banana"];
+
+// console.log(fruits);
+
+// console.log(fruits[0]);
+
+// fruits[3]= "coconut"; == fruits.push("coconut"); add element to end
+
+// fruits.pop(); remove last element
+
+// fruits.unshift("mango"); add an element to begining
+
+//fruits.shift(); remove first element
+
+// let numOfFruits = fruits.length; length of array
+// let index = fruits.indexOf("Coconut"); Returns the indexx of that element
+
+//for loop
+// for(i=0;i<fruits.length;i++){
+//     console.log(i);
+// }
+
+// for(let fruit of fruits){
+//     console.log(fruit);
+// }
+
+// fruits.sort();
+// fruits.sort().reverse();
+console.log()
